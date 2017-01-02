e0578c53edca10806800255a9b8819beb1db18d1
sam <shubham.mankhand@gmail.com>
2017-01-01 16:01:33 -0800

    bug fix

--- agentcow/cow.go
+++ agentcow/cow.go
@@ -4,7 +4,16 @@ import (
   "fmt"
   "os"
   "strings"
+  "container/list"
 )
+
+
+type workItem struct {
+  duration  int
+  cost      int
+}
+
+var workQueue list.List
 
 const port = 23432;
 
@@ -31,22 +40,29 @@ func main() {
 
   cows = make([]string, len(allcows) - 1)
 
+  fn := 0
+
   for i,j := 0, 0; i < len(allcows); i++ {
-     if (allcows[i] != myip) {
+
+    if allcows[i] == myip {
+       fn = 1
+    } else if j < len(cows) {
         cows[j] = allcows[i]
         j++
      }
-
-     if (j >= len(cows)) {
-       fmt.Printf("%s does not exist in the list of known cows. Enter a known IP.\n", myip)
-       os.Exit(1)
-     }
-
    }
 
+   if fn != 1 {
+     fmt.Printf("%s does not exist in the list of known cows. Enter a known IP.\n", myip)
+     os.Exit(1)
+   }
+
+
+  fmt.Println("Other cows in herd:")
   for i := 0; i < len(cows); i++ {
     fmt.Println(cows[i])
   }
 
+  workQueue.PushBack(workItem{1, 1})
 
 }
