c553b12468938ad56082ad5cd746062262b7031e
sam <shubham.mankhand@gmail.com>
2017-01-01 20:34:10 -0800

    Added Dockerfile.scratch and build.sh

---
+++ agentcow/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+dockeruser="shubhamat"
+appname="cow"
+
+
+if [ "$(uname -m | grep -o arm)" = "arm" ]
+then
+    arch="arm"
+    image="armhf/golang:1.6"
+else
+    arch="x86"
+    image="golang:alpine"
+fi
+dockerimgname=$dockeruser"/"$appname"_"$arch
+echo "Using image: $image"
+echo "Compiling $appname for $arch..."
+sudo docker run --rm -it -v "$(pwd)":/gocode -e "GOPATH=/gocode" -w /gocode $image sh -c "CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags=\"-s\" -o $appname"
+echo "Building $dockerimgname docker image..."
+sudo docker build  -f Dockerfile.scratch -t $dockerimgname .
+
+echo "Built image:"
+sudo docker images $dockerimgname
