0c586ae5e607ffc829e5d11ba7a7fda9e015c0ef
sam <shubham.mankhand@gmail.com>
2017-01-14 00:55:02 -0800

    added flags

--- hcrux/hcrux.go
+++ hcrux/hcrux.go
@@ -6,11 +6,25 @@ import (
 	"os"
 )
 
+type node struct {
+	id  string
+	gps string
+	ip  string
+}
+
+type piece struct {
+	hash   string
+	start  int
+	length int
+}
+
+var daemon = flag.Bool("daemon", false, "Launch daemon")
 var split = flag.Bool("split", false, "Split the file")
 var join = flag.Bool("join", false, "Join the file")
 var list = flag.Bool("listnodes", false, "List nodes in the viccinity")
 var mode = flag.String("mode", "GPS", "mode to determine what determines the viccinity. GPS coords or Bluetooth connection")
 var dist = flag.Int("distance", 100, "distance in meters to determine how close the nodes should be for a file to be join")
+var nodes = flag.String("nodes", "", "node id's of nodes where the pieces of a split file will be stored")
 
 var op string
 var fname string
@@ -35,18 +49,24 @@ func parseArgs() {
 		op = "list"
 		allops++
 	}
+	if *daemon {
+		op = "daemon"
+		allops++
+	}
 	if allops != 1 {
-		fmt.Println("One and only one operation among --listnodes, --split or --join should be specified")
+		fmt.Println("One and only one operation among --daemon, --listnodes, --split or --join should be specified")
 		usage()
 	}
 
 	fname = flag.Arg(0)
-	if op != "list" && fname == "" {
+	if (op == "split" || op == "join") && fname == "" {
 		fmt.Println("filename should be provided with a --split or --join operation")
 		usage()
 	}
 
 	switch op {
+	case "daemon":
+		daemonize()
 	case "split":
 		splitFile()
 	case "join":
@@ -54,6 +74,10 @@ func parseArgs() {
 	case "list":
 		listNodes()
 	}
+}
+
+func daemonize() {
+	fmt.Printf("Launching hcrux daemon...\n")
 }
 
 func splitFile() {
@@ -68,13 +92,19 @@ func listNodes() {
 	fmt.Printf("Searching for nodes...\n")
 }
 
+func calculateHash() {
+	/*md5 hash of file contents*/
+}
+
 func usage() {
 	fmt.Println("\n\nUsage: hcrux [OPTIONS] [filename]")
 	fmt.Println("OPTIONS:")
+	fmt.Printf("--daemon\n\tLaunch the hcrux daemon. Each node should have a daemon running.\n")
 	fmt.Printf("--split\n\tsplit the file into multiple pieces\n")
 	fmt.Printf("--join\n\tsearch and build the files if all the pieces are viccinity\n")
 	fmt.Printf("--mode=GPS|BT\n\tmode to determine what determines the viccinity. GPS coords or Bluetooth connection\n")
 	fmt.Printf("--distance=<meters>\n\tdistance in meters to determine how close the nodes should be for a file to be joined\n")
-	fmt.Printf("--listnodes\n\t list nodes in the proximity as determined by --mode and/or --distance\n")
+	fmt.Printf("--listnodes\n\tlist nodes in the proximity as determined by --mode and/or --distance\n")
+	fmt.Printf("--nodes=nodeid1[,nodeid2..]\n\tnode id's of nodes where the pieces of a split file will be stored\n")
 	os.Exit(1)
 }
